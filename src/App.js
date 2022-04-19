47199ecbbf9410ee8a6e7c1c712e5d0181faac21
theItalianDev <italiandev@protonmail.com>
2022-04-19 16:51:15 +0200

    starter template

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-// import Login from "./components/login";
-import Dashboard from "./components/dashboard";
-
 function App() {
+  const isAuthenticated = false;
   return (
-    <>
-      {/* <Login /> */}
-      <Dashboard />
-    </>
+    <div className="w-screen h-screen py-20 flex flex-col items-center bg-gradient-to-br from-blue-200 to-blue-400">
+      {!isAuthenticated ? (
+        <button className="bg-white hover:bg-gray-100 px-6 py-2 rounded-md text-xl text-blue-500 font-bold transitiond duration-300">
+          Walletconnect Auth
+        </button>
+      ) : (
+        <div className="flex flex-col itmes-center gap-2">
+          <p className="font-bold">
+            Wallet address: 00000000000000000000000000000
+          </p>
+          <p className="font-bold">Chain: Rinkeby Testnet</p>
+          <button className="bg-white hover:bg-gray-100 px-6 py-2 rounded-md text-xl text-blue-500 font-bold transitiond duration-300">
+            Logout
+          </button>
+        </div>
+      )}
+    </div>
   );
 }
 
